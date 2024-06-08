a325998f39c9ba209ba737355816ce8f5a312cfc
hayper151515 <154877146+hayper151515@users.noreply.github.com>
2024-06-08 11:52:47 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -420,7 +420,7 @@ echo -e "${Red}- 定制 ROM 包名"
 md5=$(md5sum "$GITHUB_WORKSPACE"/zip/miui_${device}_${port_os_version}.zip)
 echo "MD5=${md5:0:32}" >>$GITHUB_ENV
 zip_md5=${md5:0:10}
-rom_name="HOUJI_SHALLOW_${port_os_version}_${zip_md5}_${android_version}.0.zip"
+rom_name="HOUJI_ShaloW_${port_os_version}_${zip_md5}_${android_version}.0.zip"
 sudo mv "$GITHUB_WORKSPACE"/zip/miui_${device}_${port_os_version}.zip "$GITHUB_WORKSPACE"/zip/"${rom_name}"
 echo "rom_name=$rom_name" >>$GITHUB_ENV
 ### 输出卡刷包结束
